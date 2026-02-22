0414fc1a5788a374798a0e87ce2d40d09e799cb5
ppyom <ouob77@kakao.com>
2026-02-22 13:52:16 +0900

    ✨ 기록 페이지 구현

---
+++ apps/web/components/page/review-page.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import { useMounted } from '@/hooks/use-mounted';
+import { useReviewList } from '@/hooks/use-review-list';
+import { AddReviewAction, ReviewList } from '@/components/review';
+
+export default function ReviewPage() {
+  const isMounted = useMounted();
+  const { reviewList, todayReview, handleSave, handleDelete } = useReviewList();
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return (
+    <div
+      className="py-4 h-full flex flex-col gap-4"
+      style={{ overflowAnchor: 'none' }}
+    >
+      <div className="px-4 flex-1 overflow-y-auto">
+        <ReviewList reviewList={reviewList} onDelete={handleDelete} />
+      </div>
+      <div className="px-4">
+        <AddReviewAction onSave={handleSave} initialData={todayReview} />
+      </div>
+    </div>
+  );
+}
