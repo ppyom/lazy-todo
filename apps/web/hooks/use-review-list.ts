b703ba7b0254bea56c5d4d5e701e8af3cac3b999
ppyom <ouob77@kakao.com>
2026-02-26 19:13:37 +0900

    ✨ useReviewList에 SQLite 및 Drizzle 로직 적용

--- apps/web/hooks/use-review-list.ts
+++ apps/web/hooks/use-review-list.ts
@@ -1,17 +1,28 @@
 'use client';
 
-import { useMemo } from 'react';
+import { useEffect, useMemo, useState } from 'react';
+import { eq, sql } from 'drizzle-orm';
+import { v4 as uuid } from 'uuid';
 
 import { dayjs } from '@/lib/dayjs';
-import { useLocalStorage } from '@/hooks/use-local-storage';
-import { type Review, ReviewEmoji } from '@/types/review';
+import { useDb } from '@/components/providers/database-provider';
+import { review } from '@/db/local/schema';
+import { ReviewEmoji } from '@/types/review';
 
 /**
  * Review List의 비지니스 로직을 관리하는 커스텀 훅
  * @returns review 관련 상태와 핸들러 객체
  */
 export function useReviewList() {
-  const [reviewList, setReviewList] = useLocalStorage<Review[]>('reviews', []);
+  const db = useDb();
+  const [reviewList, setReviewList] = useState<(typeof review.$inferSelect)[]>(
+    [],
+  );
+
+  const fetchReview = async () => {
+    const result = await db.select().from(review);
+    setReviewList(result);
+  };
 
   const sortedReviewList = useMemo(() => {
     return [...reviewList].sort(
@@ -25,36 +36,37 @@ export function useReviewList() {
     );
   }, [reviewList]);
 
-  const handleSave = (emoji: ReviewEmoji, comment: string) => {
-    const now = new Date().toString();
-
+  const handleSave = async (emoji: ReviewEmoji, comment: string) => {
     if (todayReview) {
       // 수정
-      setReviewList((prev) =>
-        prev.map((review) =>
-          review.id === todayReview.id
-            ? { ...review, emoji, comment, updatedAt: now }
-            : review,
-        ),
-      );
-    } else {
-      // 작성
-      setReviewList((prev) => [
-        ...prev,
-        {
-          id: Date.now().toString(),
+      await db
+        .update(review)
+        .set({
           emoji,
           comment,
-          createdAt: now,
-          updatedAt: now,
-        },
-      ]);
+        })
+        .where(
+          eq(sql`date(${review.createdAt})`, sql`date('now', 'localtime')`),
+        );
+    } else {
+      // 작성
+      await db.insert(review).values({
+        id: uuid(),
+        emoji,
+        comment,
+      });
     }
+    await fetchReview();
   };
 
-  const handleDelete = (id: string) => {
-    setReviewList((prev) => prev.filter((review) => review.id !== id));
+  const handleDelete = async (id: string) => {
+    await db.delete(review).where(eq(review.id, id));
+    await fetchReview();
   };
+
+  useEffect(() => {
+    (async () => fetchReview())();
+  }, []);
 
   return {
     reviewList: sortedReviewList,
